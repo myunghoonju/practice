99c2a85ebc8ab696a359a9dce1fbd4965c7d3d4f
myunghoonju <myunghoonju@gmail.com>
2021-07-20 01:32:07 +0900

    java 버전 업(8 -> 11)

--- src/main/java/practice/packaging/PackageObj.java
+++ src/main/java/practice/packaging/PackageObj.java
@@ -1,4 +1,8 @@
 package practice.packaging;
 
 public class PackageObj {
+    public static void main(String[] args) {
+        var localVarType = "Hello world";
+        System.out.println(localVarType);
+    }
 }
