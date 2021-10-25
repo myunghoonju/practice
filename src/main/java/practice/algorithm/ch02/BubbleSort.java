abcb627f9793dbfa9b3ad1915bf3acae35467af4
myunghoonju <myunghoonju@gmail.com>
2021-10-25 22:54:17 +0900

    create class

---
+++ src/main/java/practice/algorithm/ch02/BubbleSort.java
@@ -0,0 +1,10 @@
+package practice.algorithm.ch02;
+
+import java.io.IOException;
+// solve it & squash commit test
+public class BubbleSort {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
