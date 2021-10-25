acea281518aa1ce92fc9d6bf5592a0b468e317fa
myunghoonju <myunghoonju@gmail.com>
2021-10-25 23:12:03 +0900

    remove comment

--- src/main/java/practice/algorithm/ch02/BubbleSort.java
+++ src/main/java/practice/algorithm/ch02/BubbleSort.java
@@ -1,7 +1,7 @@
 package practice.algorithm.ch02;
 
 import java.io.IOException;
-// solve it & squash commit test
+
 public class BubbleSort {
 
     public static void main(String[] args) throws IOException {
