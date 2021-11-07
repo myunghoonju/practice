d20d0673881cf1382ba22348c499106432ab1e8d
myunghoonju <myunghoonju@gmail.com>
2021-11-07 14:20:07 +0900

    refactoring

--- src/main/java/practice/algorithm/ch02/Consecutivity.java
+++ src/main/java/practice/algorithm/ch02/Consecutivity.java
@@ -3,60 +3,41 @@ package practice.algorithm.ch02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Consecutivity {
-    public static final Scanner scanner = new Scanner(System.in);
 
-    /**
-     * 배열의 N개의 원소가 연속적인 정수 수열로 표현될 수 있는지 판단하는 함수
-     * @param data
-     * @param n
-     * @return
-     */
-    public static boolean isConsecutive(int[] data, int n)
-    {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] data = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        boolean result = isConsecutive(data, n);
+
+        if (result) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static boolean isConsecutive(int[] data, int n) {
         int maximum = data[0];
         int minimum = data[0];
 
-        for(int i = 0; i < n ; i++)
-        {
-            if(maximum < data[i])
-            {
+        for (int i = 0; i < n; i++) {
+            if (maximum < data[i]) {
                 maximum = data[i];
             }
-            if(minimum > data[i])
-            {
+            if (minimum > data[i]) {
                 minimum = data[i];
             }
         }
 
-        System.out.println(maximum);
-        System.out.println(minimum);
-        System.out.println(">>>    " + (maximum - minimum + 1));
-        if(maximum - minimum + 1 == n)
-        {
+        if (maximum - minimum + 1 == n) {
             return true;
-        }else{
+        } else {
             return false;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        int n = scanner.nextInt();
-        int[] data = new int[n];
-        for(int i = 0 ; i < n ; i ++)
-        {
-            data[i] = scanner.nextInt();
-        }
-
-        boolean result = isConsecutive(data, n);
-
-        if(result)
-        {
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
         }
     }
 }
