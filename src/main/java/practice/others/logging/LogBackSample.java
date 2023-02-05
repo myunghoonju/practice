306aa664dca217597a1762766c731e33bd042b53
MYUNGHOON JU <myunghoonju@gmail.com>
2023-02-05 16:28:06 +0900

    add log type

--- src/main/java/practice/others/logging/LogBackSample.java
+++ src/main/java/practice/others/logging/LogBackSample.java
@@ -7,14 +7,13 @@ import org.slf4j.MarkerFactory;
 @Slf4j
 public class LogBackSample {
 
-    private static final Marker marker = MarkerFactory.getMarker("TEST");
+    private static final Marker LOG_TYPE_AGENCY = MarkerFactory.getMarker("AGENCY");
 
     public static void main(String[] args) {
-        isMethod();
+       wrapIsMethod();
     }
 
-    static void isMethod() {
-        log.info(marker, "Example marked log from {}", LogBackSample.class.getSimpleName());
-        throw new RuntimeException();
+    static void wrapIsMethod() {
+        log.info(LOG_TYPE_AGENCY, "Example marked log from {}", LogBackSample.class.getSimpleName());
     }
 }
---
+++ src/main/java/practice/others/logging/LogType.java
@@ -0,0 +1,9 @@
+package practice.others.logging;
+
+import lombok.Getter;
+
+@Getter
+public enum LogType {
+    NONE,
+    AGENCY
+}
