a857418ecceb6da0ae479ec07027c6c7ae967e52
MYUNGHOON JU <myunghoonju@gmail.com>
2023-03-10 01:53:36 +0900

    fix typo

--- src/main/java/practice/others/cache/domain/AgencyInfoService.java
+++ src/main/java/practice/others/cache/domain/AgencyInfoService.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import practice.others.cache.domain.model.AgencyDto;
-import practice.others.cache.domain.model.UpdateAgencyInfoDto;
 import practice.others.secret.encryption.CipherUtil;
 
 import javax.persistence.EntityManager;
