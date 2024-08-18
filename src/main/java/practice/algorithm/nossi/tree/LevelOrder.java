187f53facb792f3578d158e3f8cc02d121b72f98
MyungHoon Ju <myunghoonju@gmail.com>
2024-08-18 16:22:51 +0900

    level order

---
+++ src/main/java/practice/algorithm/nossi/tree/LevelOrder.java
@@ -0,0 +1,42 @@
+package practice.algorithm.nossi.tree;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+public class LevelOrder {
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20));
+    root.right.left = new TreeNode(15);
+    root.right.right = new TreeNode(7);
+
+    levelOrder(root).forEach(System.err::println);
+  }
+
+  static Set<Integer> levelOrder(TreeNode root) {
+    if (root == null) {
+      return Set.of();
+    }
+
+    Set<Integer> visited = new HashSet<>();
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      TreeNode current = queue.remove();
+      visited.add(current.val);
+
+      if (current.left != null) {
+        queue.add(current.left);
+      }
+
+      if (current.right != null) {
+        queue.add(current.right);
+      }
+    }
+
+    return visited;
+  }
+}
