2a92be66d5e70f824b1a1adacb79c20c32bf3b20
MyungHoon Ju <myunghoonju@gmail.com>
2024-11-11 01:09:55 +0900

    tidy up

--- src/main/java/practice/others/multipleDb/domain/CactusRepository.java
+++
@@ -1,5 +0,0 @@
-package practice.others.multipleDb.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CactusRepository extends JpaRepository<Cactus, Long> { }
--- src/main/java/practice/others/multipleDb/product/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package practice.others.multipleDb.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import practice.others.multipleDb.product.domain.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> { }
