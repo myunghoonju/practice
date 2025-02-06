2007ade33cc7be35827aaa2edf5d4d3384784da0
MyungHoon Ju <myunghoonju@gmail.com>
2025-02-07 02:31:39 +0900

    pass

--- src/main/java/practice/algorithm/nossi/Leet1091.java
+++ src/main/java/practice/algorithm/nossi/Leet1091.java
@@ -6,47 +6,51 @@ import java.util.Queue;
 // https://leetcode.com/problems/shortest-path-in-binary-matrix/description
 public class Leet1091 {
 
-  private static final int[][] dir = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+  private static final int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }, { 1, -1 }, { -1, 1 }, { -1, -1 }, { 1, 1 } };
+
 
   private static int solWithBfs(int [][] grid) {
-    int length = grid.length;
+    int x = grid.length;
+    int y = grid[0].length;
 
-    if (grid[0][0] == 1 || grid[length -1][length -1] == 1) {
+    if (grid[0][0] == 1 || grid[x-1][y-1] == 1) {
       return -1;
     }
 
+    boolean[][] visited = new boolean[x][y];
+    visited[0][0] = true;
     Queue<int[]> queue = new LinkedList<>();
-    queue.offer(new int[]{0, 0});
-    grid[0][0] = -1;
-    int ans = 0;
+    queue.add(new int[]{0, 0});
+    int length = 1;
     while (!queue.isEmpty()) {
-      ans++;
-      for (int i = queue.size(); i > 0 ; i--) {
-        int[] at = queue.poll();
-        int row = at[0];
-        int col = at[1];
-
-        if (row == length - 1 && col == length - 1) {
-          return ans;
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        int[] polled = queue.poll();
+        if (polled[0] == x - 1 && polled[1] == y - 1) {
+          return length;
         }
-
-        for (int[] dir : dir) {
-          int newRow = row + dir[0];
-          int newCol = col + dir[1];
-          if (newRow < 0 || newRow == length || newCol < 0 || newCol == length) {
-            continue;
+        for (int j = 0; j < dir.length; j++) {
+          int newX = dir[j][0] + polled[0];
+          int newY = dir[j][1] + polled[1];
+          if (newX >= 0 &&
+              newY >= 0 &&
+              newX < x &&
+              newY < y &&
+              !visited[newX][newY] &&
+              grid[newX][newY] == 0) {
+            queue.add(new int[]{newX, newY});
+            visited[newX][newY] = true;
           }
-
-          if (grid[newRow][newCol] == 0) {
-            continue;
-          }
-
-          grid[newRow][newCol] = -1;
-          queue.offer(new int[]{newRow, newCol});
         }
       }
+      length++;
     }
 
     return -1;
+  }
+
+  public static void main(String[] args) {
+    int[][] g = {{ 0,0,0}, { 1,1,0}, { 1,1,0}};
+    System.out.println(solWithBfs(g));
   }
 }
