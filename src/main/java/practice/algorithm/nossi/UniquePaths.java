263d32fb300c2f93d0c18c0977c6ad98d90ff2f8
MyungHoon Ju <myunghoonju@gmail.com>
2025-02-18 02:05:02 +0900

    add memoization & recursion

--- src/main/java/practice/algorithm/nossi/UniquePaths.java
+++ src/main/java/practice/algorithm/nossi/UniquePaths.java
@@ -1,7 +1,43 @@
 package practice.algorithm.nossi;
 
-public class UniquePaths {
+import java.util.ArrayList;
+import java.util.List;
 
+// https://www.geeksforgeeks.org/count-possible-paths-top-left-bottom-right-nxm-matrix/
+public class UniquePaths {
+  
+  // Memoization(top-down)
+  public int sol(int row, int col) {
+    List<List<Integer>> memo = new ArrayList<>();
+    for (int i = 0; i <= row; i++) {
+      List<Integer> r = new ArrayList<>();
+      for (int j = 0; j <= col; j++) {
+        r.add(0);
+      }
+
+      memo.add(r);
+    }
+
+    return cnt(row, col, memo);
+  }
+
+  static int cnt(int row,
+                 int col,
+                 List<List<Integer>> memo) {
+    if (row == 1 || col == 1) {
+      memo.get(row).set(col, 1);
+      return 1;
+    }
+
+    if (memo.get(row).get(col) == 0) {
+      int paths = cnt(row - 1, col, memo) + cnt(row, col - 1, memo);
+      memo.get(row).set(col, paths);
+    }
+
+    return memo.get(row).get(col);
+  }
+
+  // Tabulation(bottom-up)
   static int solution(int row, int col) {
     int[][] path = new int[row][col];
     for (int i = 0; i < row; i++) {
@@ -19,4 +55,15 @@ public class UniquePaths {
     }
     return path[row - 1][col - 1];
   }
+
+  // time exceeded
+  static int solutionRecursion(int row, int col) {
+    if (row == 1 || col == 1) {
+      return 1;
+    }
+
+    return solutionRecursion(row - 1, col) +
+           solutionRecursion(row, col - 1);
+  }
+
 }
