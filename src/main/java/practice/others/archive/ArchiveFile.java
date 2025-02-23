190aea2817784081134a48edfee1bd2ba3b4b906
MyungHoon Ju <myunghoonju@gmail.com>
2025-02-24 00:47:58 +0900

    test read content

---
+++ src/main/java/practice/others/archive/ArchiveFile.java
@@ -0,0 +1,97 @@
+package practice.others.archive;
+
+import com.tc.util.io.FileUtils;
+import org.springframework.util.FileSystemUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+// EUC-KR files
+public class ArchiveFile {
+
+  private static final String DIR = "src/main/resources/archive";
+
+  public static void main(String[] args) {
+    try {
+      LocalDate now = LocalDate.now();
+      System.out.println(now.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+      System.out.println();
+      //unzip();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public static void unzip(List<String> names, String fileDate, String zipName) throws Exception {
+    byte[] buffer = new byte[1024];
+    File destDir = new File(DIR + "/" + fileDate);
+    ZipInputStream zis = new ZipInputStream(new FileInputStream(DIR + "/" + fileDate + "/" + zipName));
+    ZipEntry entry = zis.getNextEntry();
+    while (entry != null) {
+      names.add(entry.getName());
+      File newFile = file(destDir, entry);
+      if (entry.isDirectory()) {
+        validDirectory(newFile);
+        return;
+      }
+
+      File parent = newFile.getParentFile();
+      validDirectory(parent);
+
+      FileOutputStream stream = new FileOutputStream(newFile);
+      int len;
+      while ((len = zis.read(buffer)) > 0) {
+        stream.write(buffer, 0, len);
+      }
+      stream.close();
+
+      entry = zis.getNextEntry();
+    }
+
+    zis.closeEntry();
+    zis.close();
+  }
+
+  static File file(File whereTo, ZipEntry entry) throws Exception {
+    File target = new File(whereTo, entry.getName());
+    String whereToPath = whereTo.getCanonicalPath();
+    String filePath = target.getCanonicalPath();
+    if (!filePath.startsWith(whereToPath + File.separator)) {
+      throw new IOException("zip slip detected: " + entry.getName());
+    }
+
+    return target;
+  }
+
+  static void validDirectory(File file) {
+    if (!file.isDirectory() && !file.mkdirs()) {
+      throw new RuntimeException("unable to create directory: " + file);
+    }
+  }
+
+  static void read(ReceiveData d, String name) {
+    String s = d.getFilePath().replace(d.getFileName(), name);
+    try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(s), Charset.forName("EUC-KR"))) {
+      bufferedReader.lines().forEach(l -> System.out.println(l.replace("|", " ")));
+    } catch (Exception e) {
+      System.err.println("fail to read lines " + e.getMessage());
+    }
+  }
+
+  static void delete(String dir) {
+    FileSystemUtils.deleteRecursively(new File(DIR + "/" + dir));
+  }
+}
--- src/main/java/practice/others/archive/UpdateClient.java
+++ src/main/java/practice/others/archive/UpdateClient.java
@@ -1,8 +1,11 @@
 package practice.others.archive;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UpdateClient {
+
+  static final List<String> NAMES = new ArrayList<>();
 
   public static void main(String[] args) {
     new UpdateClient().info();
@@ -10,19 +13,28 @@ public class UpdateClient {
 
   public void info() {
     Receiver receiver = new Receiver();
-    receiver.setFilePath("/Users/ju/Downloads");
-    receiver.setCreateDateDirectory(ADSUtils.YYMMDD);
+    receiver.setFilePath("src/main/resources/archive");
+    receiver.setCreateDateDirectory(ADSUtils.YYYYMMDD);
 
     try {
-      ReceiveDatas receiveDatas = receiver.receiveAddr("U01TX0FVVEgyMDI1MDIyMDIzNDAwNDExNTQ5MDU=", "D", "100001", "N", "20250220", "20250220");
-      if (receiveDatas.getResult() != 0) {
+      ReceiveDatas receiveDatas = receiver.receiveAddr("U01TX0FVVEgyMDI1MDIyMDIzNDAwNDExNTQ5MDU=", "D", "100001", "N", "20250222", "20250222");
+      if (receiveDatas.getReceiveDatas() != null) {
         if (receiveDatas.getResult() == -1) {
           System.out.println("error");
         }
 
         List<ReceiveData> receiveDatas1 = receiveDatas.getReceiveDatas();
-        receiveDatas1.stream().forEach(d -> System.out.println("- > " + d));
+        receiveDatas1.forEach(d -> {
+          try {
+            ArchiveFile.unzip(NAMES, d.getFileDate(), d.getFileName());
+            NAMES.forEach(name -> ArchiveFile.read(d, name));
+          } catch (Exception e) {
+            throw new RuntimeException(e);
+          }
+        });
       }
+
+      ArchiveFile.delete("20250222");
     } catch (Exception e) {
       System.out.println(e.getMessage());
     }
