0d2259c5539d3e3904a282dae1ba875de5db387c
MyungHoon Ju <myunghoonju@gmail.com>
2025-03-18 00:34:00 +0900

    add test

--- src/main/java/practice/algorithm/nossi/NetworkDelayTime.java
+++ src/main/java/practice/algorithm/nossi/NetworkDelayTime.java
@@ -12,17 +12,22 @@ import java.util.PriorityQueue;
 public class NetworkDelayTime {
 
   public static void main(String[] args) {
-    System.out.println(sol(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2));
+    int[][] times = {
+            {2, 1, 1}, // 2 to 1 cost 1
+            {2, 3, 1}, // 2 to 3 cost 1
+            {3, 4, 1}  // 3 to 4 cost 1
+    };
+
+    System.out.println(sol(times, 4, 2));
   }
 
   static int sol(int[][] times, int n, int k) {
     Map<Integer, List<int[]>> map = new HashMap<>();
-    for (int[] edge : times) {
-      map.putIfAbsent(edge[0], new ArrayList<>());
-      map.get(edge[0]).add(new int[]{edge[1], edge[2]});
+    for (int[] time : times) {
+      map.putIfAbsent(time[0], new ArrayList<>());
+      map.get(time[0]).add(new int[]{time[1], time[2]});
     }
-
-    PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(it -> it[1]));
+    PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(j -> j[1]));
     queue.offer(new int[]{k, 0});
     boolean[] visited = new boolean[n + 1];
     while (!queue.isEmpty()) {
@@ -30,10 +35,12 @@ public class NetworkDelayTime {
       if (visited[current[0]]) {
         continue;
       }
+
       visited[current[0]] = true;
       if (--n == 0) {
         return current[1];
       }
+
       if (map.containsKey(current[0])) {
         for (int[] next : map.get(current[0])) {
           queue.offer(new int[]{next[0], current[1] + next[1]});
