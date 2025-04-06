b63769138409b432300fb7d06deb5631d5ece44c
MyungHoon Ju <myunghoonju@gmail.com>
2025-04-06 21:46:31 +0900

    key and lock

--- src/main/java/practice/algorithm/nossi/KeyLock.java
+++ src/main/java/practice/algorithm/nossi/KeyLock.java
@@ -3,7 +3,11 @@ package practice.algorithm.nossi;
 public class KeyLock {
 
   public static void main(String[] args) {
+    int[][] key = {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}};
+    int[][] lock = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
 
+    System.out.println(solution(key, lock));
+    System.out.println(sol2(key, lock));
   }
 
   static boolean solution(int[][] key, int[][] lock) {
@@ -24,12 +28,15 @@ public class KeyLock {
                       int[][] key, int[][] lock) {
     int n = lock.length;
     int m = key.length;
-    for (int rot = 0; rot < 4; rot++) {
-      boolean match = true;
 
-      // loop labeling
-      outer:
+    for (int rot = 0; rot < 4; rot++) {
+      boolean fin = false;
+      boolean match = true;
       for (int r = 0; r < n; r++) {
+        if (fin) {
+          break;
+        }
+
         for (int c = 0; c < n; c++) {
           int keyPoint = 0;
           int keyCol = c + offsetCol;
@@ -41,7 +48,8 @@ public class KeyLock {
           if ((lock[r][c] == 1 && keyPoint == 1) ||
               (lock[r][c] == 0 && keyPoint == 0)) {
             match = false;
-            break outer;
+            fin = true;
+            break;
           }
         }
       }
@@ -54,15 +62,67 @@ public class KeyLock {
     return false;
   }
 
-  static int keyPoint(int[][] key, int keyCol, int keyRow, int rot) {
+  private static int keyPoint(int[][] key, int keyCol, int keyRow, int rot) {
     int m = key.length;
-    switch (rot) {
-      case 0: return key[keyRow][keyCol];
-      case 1: return key[keyCol][m-1-keyRow];
-      case 2: return key[m-1-keyRow][m-1-keyCol];
-      case 3: return key[m-1-keyCol][keyRow];
+    return switch (rot) {
+      case 0 -> key[keyRow][keyCol];
+      case 1 -> key[keyCol][m - 1 - keyRow];
+      case 2 -> key[m - 1 - keyRow][m - 1 - keyCol];
+      case 3 -> key[m - 1 - keyCol][keyRow];
+      default -> -1;
+    };
+  }
+
+  static boolean sol2(int[][] key, int [][] lock) {
+    for (int i = 0; i < 4; i++) {
+      if (i > 0) {
+        key = rotate(key);
+      }
+
+      for (int x = -(lock.length - 1); x < (lock.length - 1); x++) {
+        for (int y = -(lock.length - 1); y < (lock.length - 1); y++) {
+          if (match(key, lock, x, y)) {
+            return true;
+          }
+        }
+      }
     }
 
-    return -1;
+    return false;
+  }
+
+  static int[][] rotate(int[][] key) {
+    int[][] res = new int[key.length][key.length];
+    for (int i = 0; i < key.length; i++) {
+      for (int j = 0; j < key.length; j++) {
+        res[i][j] = key[key.length - 1 - j][i];
+      }
+    }
+
+    return res;
+  }
+
+  static boolean match(int [][] key,
+                int [][] lock,
+                int x,
+                int y) {
+    for (int i = 0; i < lock.length; i++) {
+      for (int j = 0; j < lock.length; j++) {
+        if (i + x < 0 ||
+            j + y < 0 ||
+            i + x >= key.length ||
+            j + y >= key.length) {
+          if (lock[i][j] == 0) {
+            return false;
+          }
+        } else {
+          if (lock[i][j] == key[x + i][y + j]) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
   }
 }
