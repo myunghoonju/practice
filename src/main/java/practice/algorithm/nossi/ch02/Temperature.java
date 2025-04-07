997dbe6dd3a22874df334a8679df9a85513b001d
MyungHoon Ju <myunghoonju@gmail.com>
2025-04-08 02:48:43 +0900

    Temperature

---
+++ src/main/java/practice/algorithm/nossi/ch02/Temperature.java
@@ -0,0 +1,29 @@
+package practice.algorithm.nossi.ch02;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Temperature {
+
+  public static void main(String[] args) {
+    int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+    Arrays.stream(sol(temperatures)).forEach(System.out::println);
+  }
+
+  static int[] sol(int[] temperatures) {
+    Deque<Integer> stack = new ArrayDeque<>();
+    int[] ans = new int[temperatures.length];
+    for (int day = 0; day < temperatures.length; day++) {
+      while (!stack.isEmpty() &&
+             temperatures[stack.peek()] < temperatures[day]) {
+        int prev = stack.pop();
+        ans[prev] = day - prev;
+      }
+
+      stack.push(day);
+    }
+
+    return ans;
+  }
+}
