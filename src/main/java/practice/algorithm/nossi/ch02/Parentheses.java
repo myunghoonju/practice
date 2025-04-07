82d0b0fa7043c440d80132632f23a1f0ac235208
MyungHoon Ju <myunghoonju@gmail.com>
2025-04-08 02:08:04 +0900

    parentheses

---
+++ src/main/java/practice/algorithm/nossi/ch02/Parentheses.java
@@ -0,0 +1,30 @@
+package practice.algorithm.nossi.ch02;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Parentheses {
+
+  public static void main(String[] args) {
+    System.out.println(valid("(]"));
+  }
+
+  static boolean valid(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+    for (char p : s.toCharArray()) {
+      switch (p) {
+        case '(': stack.push(')'); break;
+        case '{': stack.push('}'); break;
+        case '[': stack.push(']'); break;
+        default:
+          if (!stack.isEmpty() && stack.peek() == p) {
+            stack.pop();
+          } else {
+            return false;
+          }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+}
