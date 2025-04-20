bced4e3b991be5910edbae66dd5bb27cb2da3d68
MyungHoon Ju <myunghoonju@gmail.com>
2025-04-21 00:51:23 +0900

    permutation

--- src/main/java/practice/algorithm/nossi/ch01/Permutation.java
+++ src/main/java/practice/algorithm/nossi/ch01/Permutation.java
@@ -2,8 +2,45 @@ package practice.algorithm.nossi.ch01;
 
 public class Permutation {
 
-  public static void main(String[] args) {
+  private static final int[] factorial = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
 
+  public static void main(String[] args) {
+    Permutation permutation = new Permutation();
+    System.out.println(permutation.permutation(4, 9));
   }
 
+  public String permutation(int n, int k) {
+    int[] orders = new int[n + 1];
+    search(orders, n, k, 0, 0);
+    return res(n, orders);
+  }
+
+  void search(int[] orders, int n, int k, int order, int cnt) {
+    for (int i = 1; i <= n; i++) {
+      if (orders[i] != 0) {
+        continue;
+      }
+
+      if (k > cnt + factorial[n - order - 1]) {
+        cnt += factorial[n - order - 1];
+        continue;
+      }
+
+      orders[i] = order + 1;
+      search(orders, n, k, order + 1, cnt);
+      break;
+    }
+  }
+
+  private String res(int n, int[] orders) {
+    StringBuilder b = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (orders[j] == i + 1) {
+          b.append(j);
+        }
+      }
+    }
+    return b.toString();
+  }
 }
