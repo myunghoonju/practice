b8f286638e9fc221aa1ec6a1a72db06dc8943ad8
MyungHoon Ju <myunghoonju@gmail.com>
2025-05-20 00:49:38 +0900

    second sol without support

---
+++ src/main/java/practice/algorithm/Bsearch.java
@@ -0,0 +1,44 @@
+package practice.algorithm;
+
+public class Bsearch {
+
+  public static void main(String[] args) {
+    int[] ints = {9};
+    System.out.println(search(ints, 9));
+  }
+
+  // nums are sorted asc, no duplicates
+  static int search(int[] nums, int target) {
+    if (nums == null || nums.length == 0) {
+      return -1;
+    }
+
+    if (nums.length == 1) {
+      return nums[0] == target ? 0 : -1;
+    }
+
+    // find middle value
+    int midIdx = (nums.length / 2) - 1;
+    if (nums[midIdx] == target) {
+      return midIdx;
+    }
+
+    // if small then middle find from left
+    if (nums[midIdx] > target) {
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == target) {
+          return i;
+        }
+      }
+    }
+
+    // otherwise find from right
+    for (int i = nums.length -1; i >= 0; i--) {
+      if (nums[i] == target) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
