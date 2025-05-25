68dfcfce8de3b39445a0a4aad1dc3cb28467de7f
MyungHoon Ju <myunghoonju@gmail.com>
2025-05-25 18:05:40 +0900

    balanced tree

--- src/main/java/practice/algorithm/Bsearch.java
+++ src/main/java/practice/algorithm/Bsearch.java
@@ -7,6 +7,25 @@ public class Bsearch {
     System.out.println(search(ints, 9));
   }
 
+  //todo: use recurrence as below
+  int search(int[] arr, int target, int start, int end) {
+    if (start > end) {
+      return -1;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    if (arr[mid] == target) {
+      return mid;
+    }
+
+    if (target < arr[mid]) {
+      return search(arr, target, start, mid-1);
+    }
+
+    return search(arr, target, mid+1, end);
+  }
+
   // nums are sorted asc, no duplicates
   static int search(int[] nums, int target) {
     if (nums == null || nums.length == 0) {
