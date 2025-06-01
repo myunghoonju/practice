3f3877a5fe758a9edd918adb1d7324f3d5867fdf
MyungHoon Ju <myunghoonju@gmail.com>
2025-06-01 23:09:14 +0900

    edit sol

--- src/main/java/practice/algorithm/MajorityElement.java
+++ src/main/java/practice/algorithm/MajorityElement.java
@@ -1,8 +1,8 @@
 package practice.algorithm;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class MajorityElement {
 
@@ -17,17 +17,41 @@ public class MajorityElement {
       finder.putIfAbsent(num, finder.get(num) + 1);
     }
 
-    int majority = 0;
     for (Map.Entry<Integer, Integer> entry : finder.entrySet()) {
-      if (entry.getValue() > majority) {
-        majority = entry.getKey();
+      if (entry.getValue() > nums.length / 2) {
+        return entry.getKey();
       }
     }
 
-    return majority;
+    return 0;
+  }
+
+  public int bruteForce(int[] nums) {
+    int major = nums.length / 2;
+    for (int num : nums) {
+      int cnt = 0;
+      for (int el : nums) {
+        if (el == num) {
+          cnt++;
+        }
+      }
+      if (cnt > major) {
+        return num;
+      }
+    }
+
+    return -1;
+  }
+
+  public int sort(int[] nums) {
+    Arrays.sort(nums);
+    return nums[nums.length / 2];
   }
 
   public static void main(String[] args) {
-    
+    char[] charArray = "aa".toCharArray();
+    StringBuffer sb = new StringBuffer();
+
+    System.out.println(Arrays.toString(charArray));
   }
 }
