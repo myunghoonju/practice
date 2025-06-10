8f3033ad70149b774430c90d569d12bfcda74219
MyungHoon Ju <myunghoonju@gmail.com>
2025-06-11 01:57:14 +0900

    T.T

--- src/main/java/practice/algorithm/BackspaceCompare.java
+++ src/main/java/practice/algorithm/BackspaceCompare.java
@@ -1,9 +1,28 @@
 package practice.algorithm;
 
+import java.util.Stack;
+
 public class BackspaceCompare {
 
-  public boolean sol(String s, String t) {
-    return true;
+  public static void main(String[] args) {
+    boolean sol = new BackspaceCompare().b("ab#c", "ad#c");
+    System.out.println(sol);
   }
 
+  public boolean b(String s, String t) {
+    return build(s).equals(build(t));
+  }
+
+  public String build(String str) {
+    Stack<Character> ans = new Stack();
+    for (char c : str.toCharArray()) {
+      if (c != '#') {
+        ans.push(c);
+      } else if (!ans.empty()) {
+        ans.pop();
+      }
+    }
+
+    return String.valueOf(ans);
+  }
 }
