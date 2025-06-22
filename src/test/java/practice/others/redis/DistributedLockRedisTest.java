c53711c535f1492a3080ba0a26dfdb459dc989e9
MyungHoon Ju <myunghoonju@gmail.com>
2025-06-22 22:01:31 +0900

    edit test

--- src/test/java/practice/others/redis/DistributedLockRedisTest.java
+++ src/test/java/practice/others/redis/DistributedLockRedisTest.java
@@ -1,11 +1,14 @@
 package practice.others.redis;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Import(DistributedLockService.class)
@@ -16,10 +19,25 @@ public class DistributedLockRedisTest {
 
   @Test
   public void concurrent_caching_test() {
-    for (int i = 0; i < 10; i++) {
-      Stream.of("1","2","4","5")
+    for (int i = 0; i < 4; i++) {
+      Stream.of("1","2","3","4","5")
               .parallel()
               .forEach(it -> service.test(it));
     }
+
+    assertEquals(20, service.result());
+    service.clean();
+  }
+
+  @Test
+  public void concurrent_caching_without_lock_test() {
+    for (int i = 0; i < 1; i++) {
+      Stream.of("1","2","3","4","5")
+              .parallel()
+              .forEach(it -> service.testWithoutLock(it));
+    }
+
+    assertEquals(1, service.result());
+    service.clean();
   }
 }
--- src/test/java/practice/others/redis/DistributedLockService.java
+++ src/test/java/practice/others/redis/DistributedLockService.java
@@ -21,12 +21,20 @@ public class DistributedLockService {
     this.ops = ops;
   }
 
+  public int result() {
+    return ops.get("test") != null ? ops.get("test").split("").length : 0;
+  }
+
+  public void clean() {
+    ops.getAndDelete("test");
+  }
+
   public void test(String val) {
     try {
       Lock locker1 = locker.obtain("locker");
-      if (locker1.tryLock(5_000L, TimeUnit.MILLISECONDS)) {
+      if (locker1.tryLock(10_000L, TimeUnit.MILLISECONDS)) {
         try {
-          ops.set("test", ops.get("test") == null ? "begin - " + val : ops.get("test") + val);
+          ops.set("test", ops.get("test") == null ? val : ops.get("test") + val);
           Thread.sleep(2_000L);
           log.error("set success.");
         } finally {
@@ -37,6 +45,15 @@ public class DistributedLockService {
       }
     } catch (Exception e) {
       log.error("test failed.", e);
+    }
+  }
+
+  public void testWithoutLock(String val) {
+    try {
+      ops.set("test", ops.get("test") == null ? val : ops.get("test") + val);
+      log.error("set success.");
+    } catch (Exception e) {
+      log.error("set failed.", e);
     }
   }
 }
