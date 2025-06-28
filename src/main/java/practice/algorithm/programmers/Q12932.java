2626685152a1109d194e5d8fd33ede9ba2a9294a
MyungHoon Ju <myunghoonju@gmail.com>
2025-06-28 19:16:44 +0900

    Q12932

---
+++ src/main/java/practice/algorithm/programmers/Q12932.java
@@ -0,0 +1,21 @@
+package practice.algorithm.programmers;
+
+public class Q12932 {
+
+    public static int[] solution(long n) {
+        char[] arr = new StringBuilder(Long.toString(n)).reverse().toString().toCharArray();
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i] - '0';
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] solution = solution(12345);
+        for (int j : solution) {
+            System.out.println(j);
+        }
+    }
+}
