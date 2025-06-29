fe034c6d79523199107af13b7776851b84a6fa3f
MyungHoon Ju <myunghoonju@gmail.com>
2025-06-30 00:25:43 +0900

    implementation q 49993

--- src/main/java/practice/algorithm/prog/Q49993.java
+++ src/main/java/practice/algorithm/prog/Q49993.java
@@ -1,5 +1,21 @@
 package practice.algorithm.prog;
 
+import java.util.Arrays;
+
 public class Q49993 {
-    
+
+  public int sol(String skill, String[] skillTree) {
+    return (int) Arrays.stream(skillTree)
+                       .map(s -> s.replaceAll("[^" + skill + "]", ""))
+                       .filter(skill::startsWith)
+                       .count();
+  }
+
+  public static void main(String[] args) {
+    Q49993 sol = new Q49993();
+    String skill = "CBD";
+    String[] tree = {"BACDE", "CBADF", "AECB", "BDA"};
+
+    System.out.println(sol.sol(skill, tree));
+  }
 }
