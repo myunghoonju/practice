f261b25be12068bde2b53b3c9cd5bd4411af9b34
MyungHoon Ju <myunghoonju@gmail.com>
2025-06-29 17:45:31 +0900

    brute force - 42842, 67257

---
+++ src/main/java/practice/algorithm/programmers/Q42842.java
@@ -0,0 +1,24 @@
+package practice.algorithm.programmers;
+
+public class Q42842 {
+
+    public int[] solution(int brown, int yellow) {
+        for (int width = 3; width < 5_000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int b = (width + height - 2) * 2;
+                int y = (width * height) - b;
+                if (b == brown && y == yellow) {
+                    return new int[] {width, height};
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        for (int i : new Q42842().solution(10, 2)) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/practice/algorithm/programmers/Q67257.java
@@ -0,0 +1,71 @@
+package practice.algorithm.programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Q67257 {
+
+    public static final String [][] PRECEDENCES = {
+            "+-*".split(""),
+            "+*-".split(""),
+            "-+*".split(""),
+            "-*+".split(""),
+            "*-+".split(""),
+            "*+-".split(""),
+    };
+
+    public long solution(String expression) {
+        long max = 0;
+        List<String> tokens = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(expression, "+-*", true);
+        while (tokenizer.hasMoreTokens()) {
+            tokens.add(tokenizer.nextToken());
+        }
+
+        for (String[] precedence : PRECEDENCES) {
+            List<String> copied = new ArrayList<>(tokens);
+            long val = Math.abs(calc(copied, precedence));
+            if (val > max) {
+                max = val;
+            }
+        }
+
+
+        return max;
+    }
+
+    private long calc(List<String> tokens, String[] precedence) {
+        for (String op : precedence) {
+            for (int i = 0; i < tokens.size(); i++) {
+                if (tokens.get(i).equals(op)) {
+                    long l = Long.parseLong(tokens.get(i - 1));
+                    long r = Long.parseLong(tokens.get(i + 1));
+
+                    long result = calc(l, r, op);
+
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.add(i - 1, String.valueOf(result));
+                    i -= 2;
+                }
+            }
+        }
+
+        return Long.parseLong(tokens.get(0));
+    }
+
+    private long calc(long l, long r, String op) {
+        return switch (op) {
+            case "+" -> l + r;
+            case "-" -> l - r;
+            case "*" -> l * r;
+            default -> 0;
+        };
+    }
+
+    public static void main(String[] args) {
+        new Q67257().solution("100-200*300-500+20");
+    }
+}
