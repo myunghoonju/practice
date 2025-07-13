e781ef9388ae0c267be2d4357dc236a16094bce1
MyungHoon Ju <myunghoonju@gmail.com>
2025-07-14 01:29:30 +0900

    compress string

--- src/main/java/practice/algorithm/prog/Q60057.java
+++ src/main/java/practice/algorithm/prog/Q60057.java
@@ -7,41 +7,39 @@ public class Q60057 {
 
   private List<String> split(String source, int length) {
     List<String> tokens = new ArrayList<>();
-    for (int startIdx = 0; startIdx < source.length(); startIdx+=length) {
+    for (int startIdx = 0; startIdx < source.length(); startIdx += length) {
       int endIdx = startIdx + length;
-      if (endIdx > source.length()) {
-        endIdx = source.length();
-      }
-
+      if (endIdx > source.length()) endIdx = source.length();
       tokens.add(source.substring(startIdx, endIdx));
     }
 
     return tokens;
   }
 
-  private int compress(String source,int length) {
-    StringBuffer sb = new StringBuffer();
+  private int compress(String source, int length) {
+    StringBuilder builder = new StringBuilder();
+
     String last = "";
-    int cnt = 0;
+    int count = 0;
     for (String token : split(source, length)) {
       if (token.equals(last)) {
-        cnt++;
+        count++;
       } else {
-
-        if (cnt > 1) {
-          sb.append(cnt);
-          last = token;
-          cnt = 1;
-        }
+        if (count > 1) builder.append(count);
+        builder.append(last);
+        last = token;
+        count = 1;
       }
     }
+    if (count > 1) builder.append(count);
+    builder.append(last);
 
-    return sb.length();
+    return builder.length();
   }
 
   public int solution(String s) {
     int min = Integer.MAX_VALUE;
-    for (int length = 0; length < s.length(); length++) {
+    for (int length = 1; length <= s.length(); length++) {
       int compressed = compress(s, length);
       if (compressed < min) {
         min = compressed;
@@ -49,5 +47,9 @@ public class Q60057 {
     }
 
     return min;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new Q60057().solution("aab"));
   }
 }
