fe244c7f436893abf15251a34ac683ea29c5068b
MyungHoon Ju <myunghoonju@gmail.com>
2025-09-05 01:34:12 +0900

    greedy

---
+++ src/main/java/practice/algorithm/prog/Q42862.java
@@ -0,0 +1,54 @@
+package practice.algorithm.prog;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Q42862 {
+
+  public int solution(int n,
+    int[] lost,
+    int[] reserve) {
+      Arrays.sort(lost);
+      Arrays.sort(reserve);
+
+      Set<Integer> owns = Arrays.stream(lost)
+              .boxed()
+              .collect(Collectors.toSet());
+      owns.retainAll(Arrays.stream(reserve)
+              .boxed()
+              .collect(Collectors.toSet()));
+
+      Queue<Integer> needy = new LinkedList<>();
+      for (int i : lost) {
+        needy.add(i);
+      }
+
+      int get = 0;
+      for (int r : reserve) {
+        if (owns.contains(r)) {
+          continue;
+        }
+
+        while (!needy.isEmpty() &&
+                needy.peek() < r - 1 ||
+                owns.contains(needy.peek())) {
+          needy.poll();
+        }
+
+        if (needy.isEmpty()) {
+          break;
+        }
+
+        if (needy.peek() <= r + 1) {
+          needy.poll();
+          get++;
+        }
+
+      }
+
+      return n - lost.length + owns.size() + get;
+  }
+}
