7771bb06c555259e9124e287672b9cc804e5602a
MyungHoon Ju <myunghoonju@gmail.com>
2025-10-26 21:50:11 +0900

    161

---
+++ src/main/java/practice/algorithm/leetcode/LC161.java
@@ -0,0 +1,27 @@
+void main() {
+  IO.println(isOneEditDistance("ba", "a"));
+}
+
+public boolean isOneEditDistance(String s, String t) {
+  int sl = s.length();
+  int tl = t.length();
+  if (sl > tl) {
+    return isOneEditDistance(t, s);
+  }
+
+  if (tl - sl > 1) {
+    return false;
+  }
+
+  for (int i = 0; i < sl; i++) {
+    if (s.charAt(i) != t.charAt(i)) {
+      if (sl == tl) {
+        return s.substring(i + 1).equals(t.substring(i + 1));
+      } else {
+        return s.substring(i).equals(t.substring(i + 1));
+      }
+    }
+  }
+
+  return sl + 1 == tl;
+}
