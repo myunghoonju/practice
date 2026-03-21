320cbc1f1b7f3e4b62869070ec763f884a3988d5
MyungHoon Ju <myunghoonju@gmail.com>
2026-03-21 23:07:05 +0900

    edit md files

---
+++ code-review.md
@@ -0,0 +1,8 @@
+# this is a code review template
+
+## review perspective
+- [point 1] code quality(readability, maintainability, clean code)
+- [point 2] optimize code performance
+
+## others
+[//]: # (leave here)
--- src/main/java/practice/algorithm/leetcode/LC399.java
+++ src/main/java/practice/algorithm/leetcode/LC399.java
@@ -8,48 +8,41 @@ public class LC399 {
                                double[] values,
                                List<List<String>> queries) {
     Map<String, Map<String, Double>> graph = new HashMap<>();
-    //build graph
     for (int idx = 0; idx < equations.size(); idx++) {
       List<String> eq = equations.get(idx);
       String dividend = eq.get(0);
-      String divider = eq.get(1);
+      String divisor = eq.get(1);
       double value = values[idx];
 
-      if (!graph.containsKey(dividend)) {
-        graph.put(dividend, new HashMap<>());
-      }
-
-      if (!graph.containsKey(divider)) {
-        graph.put(divider, new HashMap<>());
-      }
-
-      graph.get(dividend).put(divider, value);
-      graph.get(divider).put(divider, 1 / value);
+      graph.computeIfAbsent(dividend, key -> new HashMap<>());
+      graph.computeIfAbsent(divisor, key -> new HashMap<>());
+      graph.get(dividend).put(divisor, value);
+      graph.get(divisor).put(dividend, 1 / value);
     }
 
     double[] res = new double[queries.size()];
     for (int i = 0; i < queries.size(); i++) {
       List<String> query = queries.get(i);
       String dividend = query.get(0);
-      String divider = query.get(1);
+      String divisor = query.get(1);
 
-      if (!graph.containsKey(dividend) || !graph.containsKey(divider)) {
+      if (!graph.containsKey(dividend) || !graph.containsKey(divisor)) {
         res[i] = -1.0;
-      } else if (dividend.equals(divider)) {
+      } else if (dividend.equals(divisor)) {
         res[i] = 1.0;
       } else {
         Set<String> visited = new HashSet<>();
-        res[i] = backTrack(graph, dividend, divider, 1.0d, visited);
+        res[i] = dfs(graph, dividend, divisor, 1.0d, visited);
       }
     }
     return res;
   }
 
-  private double backTrack(Map<String, Map<String, Double>> graph,
-                           String dividend,
-                           String divider,
-                           double mul,
-                           Set<String> visited) {
+  private double dfs(Map<String, Map<String, Double>> graph,
+                     String dividend,
+                     String divider,
+                     double mul,
+                     Set<String> visited) {
     visited.add(dividend);
 
     double result = -1.0d;
@@ -63,7 +56,7 @@ public class LC399 {
           continue;
         }
 
-        result = backTrack(graph, key, divider, mul * e.getValue(), visited);
+        result = dfs(graph, key, divider, mul * e.getValue(), visited);
         if (result != -1.0d) {
           break;
         }
