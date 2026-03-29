748127d63834910b2ca8a0fa14fd565ea0e47e19
MyungHoon Ju <myunghoonju@gmail.com>
2026-03-29 21:58:16 +0900

    fix solution

--- src/main/java/practice/algorithm/leetcode/LC322.java
+++ src/main/java/practice/algorithm/leetcode/LC322.java
@@ -2,25 +2,24 @@ package practice.algorithm.leetcode;
 
 import java.util.Arrays;
 
-//fixme
 public class LC322 {
 
   public int coinChange(int[] coins, int amount) {
-    if (amount == 0) {
-      return 0;
-    }
-
-    int[] memo = new int[amount + 1];
-    Arrays.fill(memo, amount + 1);
-    memo[0] = 0;
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0] = 0;
     for (int i = 0; i <= amount; i++) {
-      for (int j = 0; j < coins.length; j++) {
-        if (coins[j] <= i) {
-          memo[i] = Math.min(memo[i], memo[i - coins[j]] + 1);
+        for (int coin : coins) {
+            if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
         }
-      }
     }
 
-    return memo[amount];
+    if (dp[dp.length - 1] == Integer.MAX_VALUE) {
+      return -1;
+    }
+
+    return dp[dp.length - 1];
   }
 }
