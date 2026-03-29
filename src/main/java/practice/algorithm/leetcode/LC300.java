5f8c04a20e0fa94578229c903d4c3eec9a744461
MyungHoon Ju <myunghoonju@gmail.com>
2026-03-30 00:30:17 +0900

    tidy up

--- src/main/java/practice/algorithm/leetcode/LC300.java
+++ src/main/java/practice/algorithm/leetcode/LC300.java
@@ -5,25 +5,16 @@ import java.util.Arrays;
 public class LC300 {
 
   public int lengthOfLIS(int[] nums) {
-    if (nums.length == 1) {
-      return  1;
-    }
-
-    int[] memo = new int[nums.length];
-    Arrays.fill(memo, 1);
+    int[] dp = new int[nums.length];
     for (int i = 0; i < nums.length; i++) {
+      dp[i] = 1;
       for (int j = 0; j < i; j++) {
         if (nums[i] > nums[j]) {
-          memo[i] = Math.max(memo[i], memo[j] + 1);
+          dp[i] = Math.max(dp[i], dp[j] + 1);
         }
       }
     }
 
-    int max = 0;
-    for (int i : memo) {
-      max = Math.max(max, i);
-    }
-
-    return max;
+    return Arrays.stream(dp).max().getAsInt();
   }
 }
