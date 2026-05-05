56e666986853c0c748fd8916b88c88d316382697
MyungHoon Ju <myunghoonju@gmail.com>
2026-05-05 15:40:25 +0900

    redo LC 200

--- src/main/java/practice/algorithm/leetcode/LC200.java
+++ src/main/java/practice/algorithm/leetcode/LC200.java
@@ -5,17 +5,24 @@ import java.util.Queue;
 
 public class LC200 {
 
-    public static final int[] dx = {0, 1, 0, -1};
-    public static final int[] dy = {1, 0, -1, 0};
+    /**
+     1. 모든 칸 순회  <br>
+     2. 방문 안 한 '1'을 만나면 → BFS 시작, 연결된 모든 '1' 방문 처리  <br>
+     3. 카운트 + 1
+    */
+
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
 
     public int numIslands(char[][] grid) {
-        boolean[][] visit = new boolean[grid.length][grid[0].length];
         int cnt = 0;
-        for (int x = 0; x < grid.length; x++) {
-            for (int y = 0; y < grid[0].length; y++) {
-                if (grid[x][y] == '1' && !visit[x][y]) {
-                    cnt++;  // 새 섬 발견
-                    search(grid, visit, x, y);  // 섬 전체 방문 처리
+        boolean[][] visit = new boolean[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visit[i][j]) {
+                    search(grid, i, j, visit);
+                    cnt++;
                 }
             }
         }
@@ -23,27 +30,28 @@ public class LC200 {
         return cnt;
     }
 
-    private void search(char[][] grid, boolean[][] visit, int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] { x, y });
+    private void search(char[][] grid,
+                        int x,
+                        int y,
+                        boolean[][] visit) {
         visit[x][y] = true;
-
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
         while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int x1 = cur[0] + dx[i];
-                int y1 = cur[1] + dy[i];
-
-                if (x1 < 0 ||
-                    y1 < 0 ||
-                    x1 > grid.length - 1 ||
-                    y1 > grid[0].length - 1) {
+            int[] xy = queue.poll();
+            for (int i = 0; i < dx.length; i++) {
+                int nx = dx[i] + xy[0];
+                int ny = dy[i] + xy[1];
+                if (nx < 0 ||
+                    ny < 0 ||
+                    nx >= grid.length ||
+                    ny >= grid[0].length) {
                     continue;
                 }
 
-                if (grid[x1][y1] == '1' && !visit[x1][y1]) {
-                    visit[x1][y1] = true;
-                    queue.add(new int[] { x1, y1 });
+                if (grid[nx][ny] == '1' && !visit[nx][ny]) {
+                    queue.add(new int[]{nx, ny});
+                    visit[nx][ny] = true;
                 }
             }
         }
