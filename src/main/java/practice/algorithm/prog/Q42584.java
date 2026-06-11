d66bf8f198bd2e2304bceb6655be9a4fe5df7cae
MyungHoon Ju <myunghoonju@gmail.com>
2026-06-12 01:44:37 +0900

    Q42584

--- src/main/java/practice/algorithm/prog/Q42584.java
+++ src/main/java/practice/algorithm/prog/Q42584.java
@@ -1,34 +1,20 @@
 package practice.algorithm.prog;
 
-import java.util.Arrays;
-import java.util.Stack;
-
 public class Q42584 {
 
   public int[] solution(int[] prices) {
-    int[] answer = new int[prices.length];
+    int n = prices.length;
+    int[] answer = new int[n];
 
-    Stack<Integer> stack = new Stack<>();
-    for (int i = 0; i < prices.length; i++) {
-      while (!stack.isEmpty() &&
-             prices[stack.peek()] > prices[i]) {
-        int top = stack.pop();
-        answer[top] = i - top;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        answer[i]++;
+        if (prices[i] > prices[j]) {
+          break;
+        }
       }
-
-      stack.push(i);
-    }
-
-    while (!stack.isEmpty()) {
-      int top = stack.pop();
-      answer[top] = prices.length - top -1;
     }
 
     return answer;
-  }
-
-  public static void main(String[] args) {
-    Q42584 q = new Q42584();
-    System.out.println(Arrays.toString(q.solution(new int[]{1,2,3,2,3})));
   }
 }
