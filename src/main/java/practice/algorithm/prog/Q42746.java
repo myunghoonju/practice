b6317ea1d65bf8e55b47708a7a4758028e489750
MyungHoon Ju <myunghoonju@gmail.com>
2026-07-14 15:29:01 +0900

    Q42746

--- src/main/java/practice/algorithm/prog/Q42746.java
+++ src/main/java/practice/algorithm/prog/Q42746.java
@@ -1,19 +1,29 @@
 package practice.algorithm.prog;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class Q42746 {
 
   public String solution(int[] numbers) {
-    return Arrays.stream(numbers)
-                 .mapToObj(String::valueOf)
-                 .sorted((w1, w2) -> {
-      int a = Integer.parseInt(w1 + w2);
-      int b = Integer.parseInt(w2 + w1);
-      return b - a;
+    Integer[] boxed = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
 
-    }).collect(Collectors.joining(""))
-      .replaceAll("^0+", "0");
+    Arrays.sort(boxed, (a, b) -> {
+      String ab = "" + a + b;
+      String ba = "" + b + a;
+      return ba.compareTo(ab); // "ba"가 더 크면 양수 -> a가 뒤로 밀림 -> 결과적으로 큰 조합이 앞으로
+    });
+
+    // 3. 정렬된 순서대로 이어붙이기
+    StringBuffer sb = new StringBuffer();
+    for (int n : boxed) {
+      sb.append(n);
+    }
+
+    // 4. 경계 케이스: 맨 앞이 '0'이면 전부 0이라는 뜻 -> "0" 반환
+    if (sb.charAt(0) == '0') {
+      return "0";
+    }
+
+    return sb.toString();
   }
 }
