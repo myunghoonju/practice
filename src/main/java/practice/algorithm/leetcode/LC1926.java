909567e742c9411c065da6dd79cf6d0bac631f8f
MyungHoon Ju <myunghoonju@gmail.com>
2026-07-29 01:04:48 +0900

    bfs

--- src/main/java/practice/algorithm/leetcode/LC1926.java
+++ src/main/java/practice/algorithm/leetcode/LC1926.java
@@ -9,51 +9,46 @@ public class LC1926 {
     private static final int[] dy = {1, 0, -1, 0};
 
     public int nearestExit(char[][] maze, int[] entrance) {
+        int rows = maze.length, cols = maze[0].length;
+        boolean[][] visited = new boolean[rows][cols];   // 2번: maze 크기 기준
         Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
+
+        queue.add(new int[]{entrance[0], entrance[1]});  // 3번: entrance부터 시작
         visited[entrance[0]][entrance[1]] = true;
-        queue.add(new int[] {entrance[0], entrance[1], 0});
-        for (int i = 0; i < maze.length; i++) {
-            for (int i1 = 0; i1 < maze[0].length; i1++) {
-                if (maze[i][i1] == '+') {
-                    visited[i][i1] = true;
-                }
-            }
-        }
 
-        return findExit(queue, maze, visited);
-    }
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            steps++;                     // 레벨(=한 걸음)마다 1씩 증가
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                int[] cur = queue.poll();
+                for (int d = 0; d < 4; d++) {
+                    int nr = cur[0] + dx[d];
+                    int nc = cur[1] + dy[d];
 
-    private int findExit(Queue<int[]> queue,
-                         char[][] maze,
-                         boolean[][] visited) {
-            while (!queue.isEmpty()) {
-                int[] xy = queue.poll();
-                int x = xy[0];
-                int y = xy[1];
-                int step = xy[2];
-                for (int i = 0; i < 4; i++) {
-                    int newx = x + dx[i];
-                    int newy = y + dy[i];
-                    if (newx < 0 ||
-                        newy < 0 ||
-                        newx >= maze.length ||
-                        newy >= maze[0].length ||
-                        visited[newx][newy]) {
+                    // TODO: nr, nc가 범위 안인지, 벽('+')이 아닌지, 이미 방문했는지 확인
+                    if (nr < 0 ||
+                        nr >= rows ||
+                        nc < 0 ||
+                        nc >= cols ||
+                        maze[nr][nc] == '+' ||
+                        visited[nr][nc]) {
                         continue;
                     }
 
-                    if (maze[newx][newy] == '.' && !visited[newx][newy]) {
-                        visited[newx][newy] = true;
-                        queue.add(new int[] {newx, newy, step + 1});
-                        if (newx == 0 ||
-                            newy == 0 ||
-                            newx == maze.length - 1 || newy == maze[0].length - 1) {
-                                return step + 1;
-                        }
+                    // TODO: 여기가 4번 - 이 셀이 border이고 entrance가 아니면 return steps
+                    boolean isBorder = nr == 0 || nr == rows - 1 || nc == 0 || nc == cols - 1;
+                    boolean isEntrance = nr == entrance[0] && nc == entrance[1];
+                    if (isBorder && !isEntrance) {
+                        return steps;
                     }
+
+                    // TODO: 아니면 visited 표시하고 queue.add(new int[]{nr, nc})
+                    visited[nr][nc] = true;
+                    queue.add(new int[]{nr, nc});
                 }
             }
+        }
 
         return -1;
     }
