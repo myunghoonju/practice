265bb01d75bbaff508e81dd5cd61fb4b3f237b0e
MyungHoon Ju <myunghoonju@gmail.com>
2026-08-05 01:12:06 +0900

    Revert "test"
    This reverts commit 7bac2a3b286d1ddd2604175f460c86d78a0357f9.

--- src/main/java/practice/others/mongo/InsufficientStockException.java
+++
@@ -1,8 +0,0 @@
-package practice.others.mongo;
-
-public class InsufficientStockException extends RuntimeException {
-
-  public InsufficientStockException(String productId, int requestedQuantity) {
-    super("Insufficient stock for product " + productId + ", requested " + requestedQuantity);
-  }
-}
--- src/main/java/practice/others/mongo/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package practice.others.mongo;
-
-public class ProductNotFoundException extends RuntimeException {
-
-  public ProductNotFoundException(String productId) {
-    super("Product not found: " + productId);
-  }
-}
