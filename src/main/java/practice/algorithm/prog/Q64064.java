be814f013e2b9c0d0b52eb5b10c0ec7ef0d58d07
MyungHoon Ju <myunghoonju@gmail.com>
2026-08-21 15:12:39 +0900

    redo

--- src/main/java/practice/algorithm/prog/Q64064.java
+++ src/main/java/practice/algorithm/prog/Q64064.java
@@ -1,69 +1,52 @@
 package practice.algorithm.prog;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Q64064 {
 
+  private final Set<Set<String>> result = new HashSet<>();
+
   public int solution(String[] user_id, String[] banned_id) {
-    Map<Integer, List<String>> ids = new HashMap<>();
-
-    for (int j = 0; j < banned_id.length; j++) {
-      for (String user : user_id) {
-        if (match(user, banned_id[j])) {
-          List<String> list = ids.getOrDefault(j, new ArrayList<>());
-          list.add(user);
-          ids.put(j, list);
-        }
-      }
-    }
-
-    return permutate(ids, banned_id.length);
+    match(0, user_id, banned_id, new HashSet<>());
+    return result.size();
   }
 
-  private int permutate(Map<Integer, List<String>> ids, int totalSlots) {
-    Set<Set<String>> results = new HashSet<>();
-    backtrack(0, totalSlots, ids, new HashSet<>(), new ArrayList<>(), results);
-    return results.size();
-  }
-
-  private void backtrack(int slot, int totalSlots,
-                         Map<Integer, List<String>> ids, Set<String> visited,
-                         List<String> current, Set<Set<String>> results) {
-    if (slot == totalSlots) {
-      results.add(new HashSet<>(current));
-
-      return;
+  private Set<String> match(int banIdx, String[] user_id, String[] banned_id, Set<String> uniqueList) {
+    if (banIdx == banned_id.length) {
+      result.add(new HashSet<>(uniqueList));
+      return  uniqueList;
     }
 
-    List<String> candidates = ids.getOrDefault(slot, new ArrayList<>());
-    for (String candidate : candidates) {
-      if (visited.contains(candidate)) {
+
+    String target = banned_id[banIdx];
+    for (String user : user_id) {
+      boolean related = true;
+      String[] splitUser = user.split("");
+      String[] splitTarget = target.split("");
+      if (user.length() != target.length()) {
         continue;
       }
 
-      visited.add(candidate);
-      current.add(candidate);
+      for (int tIdx = 0; tIdx < splitTarget.length; tIdx++) {
+        if (splitTarget[tIdx].equals("*")) {
+          continue;
+        }
 
-      backtrack(slot + 1, totalSlots, ids, visited, current,  results);
+        if (!splitTarget[tIdx].equals(splitUser[tIdx])) {
+          related = false;
+        }
+      }
 
-      visited.remove(candidate);
-      current.remove(candidate);
-    }
-  }
 
-  private boolean match(String user, String banned) {
-    if (user.length() != banned.length()) {
-      return false;
-    }
-
-    for (int i = 0; i < banned.length(); i++) {
-      char b = banned.charAt(i);
-      char u = user.charAt(i);
-      if (b != '*' && Character.toLowerCase(b) != Character.toLowerCase(u)) {
-        return false;
+      boolean preExist = uniqueList.stream().anyMatch(m -> m.equals(user));
+      if (related && !preExist) {
+        uniqueList.add(user);
+        match(banIdx + 1, user_id, banned_id, uniqueList);
+        uniqueList.remove(user);
       }
     }
 
-    return true;
+    return uniqueList;
   }
 }
